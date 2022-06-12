acac10998fb158189b565b4d7eff5e5a0ebe3eba
Ines Borges <ines@coresolucoes.com>
2022-06-12 10:46:53 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React, {useRef} from "react";
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const spanUsername = useRef();
+  const spanPassword = useRef();
+
+  async function getUsers(){
+     await fetch("https://restapitestewds.herokuapp.com/62a5a5d4eaf608f25d697d91", {method: "GET"})
+      .then((res) => {
+        return res.json();
+      })
+      .then(data => {
+        spanUsername.current.value = data.username;
+        spanPassword.current.value = data.password;
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header onLoad={getUsers} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Utilizador:</p> <span></span>
-        <p>Password:</p> <span></span>
+        <p>Utilizador:</p> <span ref={spanUsername}></span>
+        <p>Password:</p> <span ref={spanPassword}></span>
       </header>
     </div>
   );
